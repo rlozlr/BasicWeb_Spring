525e45c61c538a27e72cf50e84d6e9ffc9895cec
rlozlr <0918rlog@gmail.com>
2024-01-16 01:25:18 +0900

    WebConfig / ServletConfiguration 설정

--- basicWeb/src/main/java/com/basicWeb/www/config/ServletConfiguration.java
+++ basicWeb/src/main/java/com/basicWeb/www/config/ServletConfiguration.java
@@ -1,5 +1,29 @@
 package com.basicWeb.www.config;
 
-public class ServletConfiguration {
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+public class ServletConfiguration implements WebMvcConfigurer {
+
+	// resources 경로 설정 / 나중에 파일 업로드 경로 설정 추가
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	// 뷰 경로 설정
+	// 생성자로도 설정 가능
+	//InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		registry.viewResolver(viewResolver);
+	}
 
 }
